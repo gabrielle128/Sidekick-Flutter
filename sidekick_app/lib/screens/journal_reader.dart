58fb7b09f8145db271741b4f80037ebee4bf20ca
Dann Joseph <d4nnjoseph@gmail.com>
2023-12-13 10:06:56 +0800

    Cleaned up the changes

--- sidekick_app/lib/screens/journal_reader.dart
+++ sidekick_app/lib/screens/journal_reader.dart
@@ -15,23 +15,23 @@ class _JournalReaderScreenState extends State<JournalReaderScreen> {
   
   @override
   Widget build(BuildContext context){
-    int color_id = widget.doc['color_id'];
+    int colorId = widget.doc['color_id'];
     return Scaffold(
-      backgroundColor: AppStyle.cardsColor[color_id],
+      backgroundColor: AppStyle.cardsColor[colorId],
       appBar: AppBar(
-        backgroundColor: AppStyle.cardsColor[color_id],
+        backgroundColor: AppStyle.cardsColor[colorId],
         elevation: 0.0,
 
       ),
       body: Padding(
-        padding: EdgeInsets.all(25.0),
+        padding: const EdgeInsets.all(25.0),
         child: Column (
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(widget.doc["journal_title"], style: AppStyle.mainTitle,),
-            SizedBox(height: 4.0,),
+            const SizedBox(height: 4.0,),
             Text(widget.doc["creation_date"], style: AppStyle.mainDate,),
-            SizedBox(height: 30.0,),
+            const SizedBox(height: 30.0,),
             Text(widget.doc["journal_content"], style: AppStyle.mainContent, overflow: TextOverflow.ellipsis),
       
           ],
